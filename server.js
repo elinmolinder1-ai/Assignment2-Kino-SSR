cc218fda5f7de8a57a78c3282450e222ade57b45
elinmolindr <elinmolinder1@gmail.com>
2026-01-19 16:21:48 +0100

    Express server with handlebars

--- server.js
+++ server.js
@@ -4,8 +4,15 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { loadMovie, loadMovies } from "./SSR/lib/movies.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+//app.use("/static", express.static(path.join(__dirname, "SSR/static")));
 
 // Handlebars setup
 app.engine("handlebars", engine({
@@ -14,7 +21,24 @@ app.engine("handlebars", engine({
   partialsDir: path.join(__dirname, "SSR/templates/partials")
 }));
 
+
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "SSR/templates"));
+
+/*
+// Startsidan → skicka index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});*/
+
 app.get("/", async (req, res) => {
+  const movies = await loadMovies(); // om du vill visa filmer direkt
+  res.render("home", { movies });
+});
+
+app.get("/", async (req, res) => { const movies = await loadMovies(); res.render("home", { movies }); });
+// SSR-sidor
+app.get("/movies", async (req, res) => {
   const movies = await loadMovies();
   res.render("home", { movies });
 });
@@ -24,6 +48,8 @@ app.get("/movies/:movieId", async (req, res) => {
   res.render("movie", { movie });
 });
 
-app.use("/static", express.static("./static"));
 
-app.listen(5080);
+
+app.listen(5080, () => {
+  console.log("Servern kör på http://localhost:5080");
+});
